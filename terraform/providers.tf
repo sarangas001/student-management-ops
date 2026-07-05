1ab05da3bdb3c61ef866a7cfbdf6e9a3f3c20ce9
saranaga <sarangasama@gmail.com>
2026-07-06 00:04:45 +0530

    change the terraform

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -9,5 +9,5 @@ terraform {
 }
 
 provider "aws" {
-  region = "ap-southeast-1"
+  region = "us-east-1"
 }
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -22,7 +22,7 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "ap-southeast-1a"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true 
 
   tags = {
